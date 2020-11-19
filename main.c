f6d2fa669888df06d5caafc8028b1377a1566b60
scarfaced00 <jazmyne@naver.com>
2020-11-19 12:34:42 +0900

    ex6

--- main.c
+++ main.c
@@ -3,30 +3,17 @@
 #include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/*void main(void)
-{
-	FILE *fp=NULL;
-	
-	fp=fopen("sample.txt","w");
-	if(fp==NULL)
-	{
-		printf("파일을 못열음\n");
-		return;
-	}
-	
-	fputc('a',fp);
-	fclose(fp);
-}*/
+
 int main(int argc, char *argv[]) {
 	int i;
 	FILE *fp=NULL;
-	char word[100];
-	fp=fopen("sample.txt","w");
-	for(i=0;i<3;i++)
+	char c;
+	fp=fopen("sample.txt","r");
+	if(fp==NULL)
+		printf("파일을 못열음\n");
+	while((c=fgetc(fp))!=EOF)
 	{
-		printf("input a word:");
-		scanf("%s",word);
-		fprintf(fp,"%s\n",word);
+		putchar(c);
 	}
 	fclose(fp);
 	return 0;
