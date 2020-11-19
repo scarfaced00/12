50407f142f44092b86203a076e143dd08804fb46
scarfaced00 <jazmyne@naver.com>
2020-11-19 16:16:31 +0900

    ex6_2

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	FILE *fp=NULL;
-	char c;
-	fp=fopen("sample.txt","r");
-	if(fp==NULL)
-		printf("파일을 못열음\n");
-	while((c=fgetc(fp))!=EOF)
-	{
-		putchar(c);
+	FILE *fp;
+	char str[100];
+	fp=fopen("simple.txt","r");
+	if (fp==NULL)
+		return -1;
+	
+	while(fgets(str,100,fp)!=NULL)
+		{
+			printf("%s",str);
+		}
+		
+		fclose(fp);
+		
+		return 0;
 	}
-	fclose(fp);
-	return 0;
-}
